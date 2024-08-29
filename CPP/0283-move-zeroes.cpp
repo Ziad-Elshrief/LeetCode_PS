145ae4e7d3a9d379220e3e12563842fb25a26cb9
Ziad Elshrief <107151454+Ziad-Elshrief@users.noreply.github.com>
2024-08-29 23:51:57 +0300

    Update 0283-move-zeroes.cpp

--- CPP/0283-move-zeroes.cpp
+++ CPP/0283-move-zeroes.cpp
@@ -2,14 +2,17 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int temp;
-        for(int i=0;i<nums.size();i++){
-            for(int j=0;j<nums.size()-1-i;j++){
+        int last_index=nums.size()-1;
+        for(int i=0;i<=last_index;i++){
+            for(int j=0;j<last_index;j++){
                 if(nums[j]==0 ){
                     temp=nums[j];
                     nums[j]=nums[j+1];
                     nums[j+1]=temp;
             }
             }
+            if(nums[0] !=0 && nums[last_index]==0)
+                last_index--;
         }
     }
 };
