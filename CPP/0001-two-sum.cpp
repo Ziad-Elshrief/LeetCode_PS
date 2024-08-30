df8600f679273cf8c8668c3d83d4003926da7c86
Ziad Elshrief <107151454+Ziad-Elshrief@users.noreply.github.com>
2024-08-30 18:33:38 +0300

    Update 0001-two-sum.cpp

--- CPP/0001-two-sum.cpp
+++ CPP/0001-two-sum.cpp
@@ -2,15 +2,27 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         vector<int> Result{-1,-1};
-        for(int i=0;i<nums.size();i++){
-            for(int j=i+1;j<nums.size();j++){
-                if(nums[i]+nums[j]==target){
+        int min=nums[0];
+        int max=nums[0];
+        int i;
+        /* get max and min values from the input nums*/
+        for(i=0;i<nums.size();i++){
+            if(nums[i]>max) max=nums[i];
+            else if(nums[i]<min) min=nums[i];
+        }
+        /* calculate Target-nums[i] and if the result is in range of
+        max and min value then it may be the answer so we search for it*/
+         for(i=0;i<nums.size();i++){
+            if(target-nums[i]<=max && target-nums[i]>= min){
+                for(int j=i+1;j<nums.size();j++){
+                    if(nums[i]+nums[j]==target){
                     Result[0]=i;
                     Result[1]=j;
-                    break;
+                   return Result;
+                }
                 }
             }
-        }
+         }
         return Result;
     }
 };
