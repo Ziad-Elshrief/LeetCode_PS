4e92cfd3e6126d460d6254f8b73ab7c775a1c9a2
Ziad Elshrief <107151454+Ziad-Elshrief@users.noreply.github.com>
2024-08-31 23:25:11 +0300

    Update 0268-missing-number.cpp

--- CPP/0268-missing-number.cpp
+++ CPP/0268-missing-number.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        int result=nums.size();
+        long long int sum=0 ,original_sum=0;
         for(int i=0;i<nums.size();i++){
-            if(nums[i] != i){
-                result=i;
-                break;
-            }
+            /*as there's no need to add 0 itself 
+              adding numbers in range [1,n]*/
+            original_sum=original_sum+i+1;
+            /* adding array elements*/
+            sum+=nums[i];
         }
-        return result;
+        /*return original_sum - sum -> missing number 
+            if they are the same then missing num -> 0 */
+        return original_sum-sum;
     }
 };
